552f690e5e6ef722b1b093f3b4219f2a60324fab
Colt Steele <5498438+Colt@users.noreply.github.com>
2022-06-20 19:28:15 -0600

    starter code

---
+++ dist/utils.js
@@ -0,0 +1,10 @@
+import _ from "lodash";
+export function add(x, y) {
+    return _.add(x, y);
+}
+export function multiply(x, y) {
+    return x * y;
+}
+export function divide(x, y) {
+    return x / y;
+}
